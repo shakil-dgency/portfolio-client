165ed69beba87fd2afc3bf2080a73d1db2d899e9
shakil <shakil.dgency@gmail.com>
2024-03-07 18:10:34 +0600

    adjust some design in navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,41 +6,41 @@ import logo from "../../public/H M Hamiduzjaman - Logo.svg";
 import SearchComponent from "./NewsFeed/SearchComponent";
 
 function NavBar({ search, setSearch, isSearch }) {
-	useEffect(() => {
-		let lastScrollTop = 0;
-		let navBar = document.querySelector(".navbar");
+	// useEffect(() => {
+	// 	let lastScrollTop = 0;
+	// 	let navBar = document.querySelector(".navbar");
 
-		window.addEventListener("scroll", function () {
-			let scrollTop = window.scrollY || document.documentElement.scrollTop;
-			if (scrollTop > 70) {
-				navBar.classList.remove("bg-[#FAFAFA]");
-				navBar.classList.add("bg-[rgba(0,0,0,0.1)]");
-			} else if (scrollTop === 0) {
-				navBar.classList.add("bg-[#FAFAFA]");
-				navBar.classList.remove("bg-[rgba(0,0,0,0.1)]");
-			}
+	// 	window.addEventListener("scroll", function () {
+	// 		// let scrollTop = window.scrollY || document.documentElement.scrollTop;
+	// 		// if (scrollTop > 70) {
+	// 		// 	navBar.classList.remove("bg-[#FAFAFA]");
+	// 		// 	navBar.classList.add("bg-[rgba(0,0,0,0.1)]");
+	// 		// } else if (scrollTop === 0) {
+	// 		// 	navBar.classList.add("bg-[#FAFAFA]");
+	// 		// 	navBar.classList.remove("bg-[rgba(0,0,0,0.1)]");
+	// 		// }
 
-			// if (scrollTop > 600) {
-			// if (scrollTop > lastScrollTop) {
-			// 	navbar.style.top = "-150px";
-			// 	setServiceMenubar(false);
-			// 	setResourcesOpen(false);
-			// 	setQuizeState(false);
-			// 	setMoreState(false);
-			// } else if (scrollTop === 0) {
-			// 	if (marketingBanner === true) {
-			// 		navbar.style.top = "56px";
-			// 	} else if (marketingBanner === false) {
-			// 		navbar.style.top = "0";
-			// 	}
-			// } else if (lastScrollTop > scrollTop + 20) {
-			// 	navbar.style.top = "0";
-			// }
-			// }
+	// 		// if (scrollTop > 600) {
+	// 		// if (scrollTop > lastScrollTop) {
+	// 		// 	navbar.style.top = "-150px";
+	// 		// 	setServiceMenubar(false);
+	// 		// 	setResourcesOpen(false);
+	// 		// 	setQuizeState(false);
+	// 		// 	setMoreState(false);
+	// 		// } else if (scrollTop === 0) {
+	// 		// 	if (marketingBanner === true) {
+	// 		// 		navbar.style.top = "56px";
+	// 		// 	} else if (marketingBanner === false) {
+	// 		// 		navbar.style.top = "0";
+	// 		// 	}
+	// 		// } else if (lastScrollTop > scrollTop + 20) {
+	// 		// 	navbar.style.top = "0";
+	// 		// }
+	// 		// }
 
-			lastScrollTop = scrollTop;
-		});
-	});
+	// 		lastScrollTop = scrollTop;
+	// 	});
+	// });
 
 	return (
 		<div className="navbar bg-[#FAFAFA]  backdrop-blur-[25px] text-[#222222]  sticky top-0 z-50 w-full ">
@@ -51,7 +51,7 @@ function NavBar({ search, setSearch, isSearch }) {
 					</Link>
 				</div>
 				<div className="">
-					<ul className="flex gap-5 font-[500]">
+					<ul className="flex gap-4 font-[500]">
 						<li className=" cursor-pointer">Home</li>
 						<li className="">
 							<Link href={"/profile"}>Profile</Link>
--- src/components/NewsFeed/SearchComponent.js
+++ src/components/NewsFeed/SearchComponent.js
@@ -5,8 +5,7 @@ function SearchComponent({ search, setSearch }) {
 	return (
 		<div>
 			<div className="">
-				{/* <p className="mt-8 mb-2 font-[700]">Search</p> */}
-				<div className="relative flex max-w-[220px] ">
+				<div className="relative flex max-w-[220px] ml-3">
 					<input
 						type="text"
 						onChange={(e) => setSearch(e.target.value)}
@@ -17,19 +16,6 @@ function SearchComponent({ search, setSearch }) {
 						<IoIosSearch className="text-[24px] mr-1" />
 					</button>
 				</div>
-				{/* <p className="text-[14px] mt-2">Lorem ipsum dolor sit, amet consectetur</p> */}
-
-				{/* <div className="">
-					<p className="mt-6 mb-2 font-[700]">Subscribe</p>
-					<div className="relative flex max-w-[280px]">
-						<input
-							type="email"
-							placeholder="dgency@gmail.com"
-							className="outline-none px-2 py-[6px] w-full rounded-l-md border-[1px] border-[#ffb900] bg-transparent text-[15px]"
-						/>
-						<button className=" bg-[#ffb900] text-white px-2 rounded-r-md text-[14px] w-[90px] ">Sign Up</button>
-					</div>
-				</div> */}
 			</div>
 		</div>
 	);
